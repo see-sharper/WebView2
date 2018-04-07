ec3961734f1d6a598f3c2f0697e4888362a1f3d7
BhaumikM <bondnstock@gmail.com>
2018-04-07 00:12:33 -0400

    WebView link now opens within webview. Action bar (thought title bar at first) is now removed.

--- app/src/main/java/us/bhaumik/dev/webview/MainActivity.java
+++ app/src/main/java/us/bhaumik/dev/webview/MainActivity.java
@@ -2,6 +2,7 @@ package us.bhaumik.dev.webview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //his.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         WebView webView;
