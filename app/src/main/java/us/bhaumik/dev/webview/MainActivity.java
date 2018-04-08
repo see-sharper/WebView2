55d9fa2b70856726be60771be6dde7178da8d226
BhaumikM <bondnstock@gmail.com>
2018-04-07 23:33:38 -0400

    Handled the situation when internet is not available

--- app/src/main/java/us/bhaumik/dev/webview/MainActivity.java
+++ app/src/main/java/us/bhaumik/dev/webview/MainActivity.java
@@ -1,5 +1,8 @@
 package us.bhaumik.dev.webview;
 
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -19,15 +22,59 @@ public class MainActivity extends AppCompatActivity {
         webView = findViewById(R.id.webview1);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new InBrowser());
-        /* comment */
+
         webView.loadUrl("http://safarnama.bhaumik.us");
+
+        webView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView webView, int errorCode, String description, String failingUrl) {
+                try {
+                    webView.stopLoading();
+                } catch (Exception e) {
+                }
+
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                }
+
+                webView.loadUrl("about:blank");
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Error");
+                alertDialog.setMessage("Check your internet connection and try again.");
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, "Try Again", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                        startActivity(getIntent());
+                    }
+                });
+
+                alertDialog.show();
+                super.onReceivedError(webView, errorCode, description, failingUrl);
+            }
+        });
     }
 
     private class InBrowser extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
         }
     }
+/*
+    @Override
+    //This method is used to detect back button
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // Let the system handle the back button
+            super.onBackPressed();
+        }
+    }*/
 }
